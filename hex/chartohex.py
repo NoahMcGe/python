66c3a8c3fe9d827b005c641051b454352e985f12
noah <corphos111@gmail.com>
2019-05-14 09:49:49 -0700

    aa

--- hex/chartohex.py
+++ hex/chartohex.py
@@ -1,8 +1,16 @@
 import binascii
 noah = binascii.hexlify(b'Steve Pi')
 mcgehee = binascii.hexlify(b'Noah McGehee')
-he = "HEX"
+he = "HEX ="
+line = "————————————————————————————————————————————————————————————————————"
+print (line)
 print ("Steve Pi")
 print (he,noah)
+print (line)
 print ("Noah McGehee")
 print (he,mcgehee)
+print (line)
+a = input("Input your own text : ")
+f = binascii.hexlify(a.encode('utf-8'))
+print ("HEX =",  f)
+print (line)
