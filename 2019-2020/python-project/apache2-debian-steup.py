34efb1366eb0f0fa3f937121519190787300a4cc
NoahMcGe <35183952+NoahMcGe@users.noreply.github.com>
2020-02-07 12:29:11 -0800

    Update apache2-debian-steup.py

--- 2019-2020/python-project/apache2-debian-steup.py
+++ 2019-2020/python-project/apache2-debian-steup.py
@@ -6,7 +6,9 @@ username = getpass.getuser()
 myhost = os.uname()[1]
 
 def apache2set():
-  print("Username:"+username+" Hostname:"+myhost)
-
+	print("Username:"+username+" Hostname:"+myhost)
+	bash("cd /home/"+username+"/;mkdir http/;cd http/;mkdir logs;cd ..;chmod 755 http/ -R;")
+  bash("wget https://raw.githubusercontent.com/NoahMcGe/documents/master/apache/localhost.conf;chmod 755 localhost.conf;")
+#sed -i 's/original/new/g' file.txt
 def main():
-  apache2set()
+	apache2set()
